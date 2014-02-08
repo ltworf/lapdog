d6ff73514c2a1112993e14beecafb44eb7e1fd96
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2014-02-08 13:53:19 +0100

    ARP table object
    It really doesn't do very much, just hold ARP entries in memory,
    as strings.

---
+++ arp_table_entry.cpp
@@ -0,0 +1,35 @@
+/*
+ * lapdog - take actions on devices (dis)appearance on the LAN
+ * Copyright (C) 2014  Salvo Tomaselli <tiposchi@tiscali.it>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <string.h>
+
+#include "arp_table_entry.h"
+
+arp_table_entry::arp_table_entry(char* ip_addr,char* hw_addr) {
+    strncpy(this->hw_addr,hw_addr,sizeof(this->hw_addr));
+    strncpy(this->ip_addr,ip_addr,sizeof(this->ip_addr));
+}
+
+bool arp_table_entry::has_hw_addr(char* hw_addr) {
+    return strcmp(this->hw_addr,hw_addr)==0;
+}
+
+char* arp_table_entry::get_ip_addr() {
+    return this->ip_addr;
+}
---
+++ arp_table_entry.h
@@ -0,0 +1,33 @@
+/*
+ * lapdog - take actions on devices (dis)appearance on the LAN
+ * Copyright (C) 2014  Salvo Tomaselli <tiposchi@tiscali.it>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef ARP_TABLE_ENTRY_H
+#define ARP_TABLE_ENTRY_H
+
+class arp_table_entry {
+public:
+    arp_table_entry(char* ip_addr,char* hw_addr);
+    bool has_hw_addr(char* hw_addr);
+    char* get_ip_addr();
+private:
+    char ip_addr[16];
+    char hw_addr[18];
+};
+
+#endif // ARP_TABLE_ENTRY_H
