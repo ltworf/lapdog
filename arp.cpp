e4a4354fbd64303e5ce938ab2182aafd3d4e9bf8
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2014-02-08 13:56:57 +0100

    ARP table
    Scans the ARP table from the kernel and keeps it in memory.
    
    Code is partially copied from "arp" command.

---
+++ arp.cpp
@@ -0,0 +1,83 @@
+/*
+ * lapdog - take actions on devices (dis)appearance on the LAN
+ * Copyright (C) 2014  Salvo Tomaselli <tiposchi@tiscali.it>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <stdio.h>
+
+#include "arp.h"
+
+arp::arp() {
+    this->rescan();
+}
+
+/**
+ * Returns a string containing the corresponding IP address of the device
+ * according to the kernel's ARP table.
+ *
+ * NULL if the entry is not found.
+ *
+ * This function only checks inside a copy of the table. run rescan() to
+ * re-load the table from the kernel.
+ **/
+char* arp::find_ip_addr(char* hw_addr) {
+    for (int i=0; i<entries.size(); i++) {
+        if (entries[i].has_hw_addr(hw_addr))
+            return entries[i].get_ip_addr();
+    }
+    return NULL;
+}
+
+/**
+ * Performs a rescan of the ARP table from the kernel, cleaning up
+ * the internally stored one.
+ *
+ * The code is kind of copied from arp.c utility in net-tools
+ **/
+void arp::rescan() {
+    char ip[100];
+    char hwa[100];
+    char mask[100];
+    char line[200];
+    char dev[100];
+    int type, flags;
+    int num;
+
+    entries.clear();
+
+    FILE *file = fopen(ARP_TABLE_FILE, "r");
+
+    //Skip first header line
+    if (fgets(line, sizeof(line), file)==NULL)
+        goto cleanup;
+
+    for (; fgets(line, sizeof(line), file);) {
+        num = sscanf(line, "%s 0x%x 0x%x %100s %100s %100s\n",
+             ip, &type, &flags, hwa, mask, dev
+        );
+        if (num < 4)
+            break;
+
+        arp_table_entry new_entry = arp_table_entry(ip,hwa);
+        entries.push_back(new_entry);
+    }
+
+cleanup:
+    fclose(file);
+}
+
+
